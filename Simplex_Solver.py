6dda1fffbc7621a8ae21bd93f0688d3fceb565f2
Iyad Walweel <iyad.walwel@gmail.com>
2022-05-25 17:49:29 +0200

    Infeasibality by Two-Phase

--- Simplex_Solver.py
+++ Simplex_Solver.py
@@ -16,15 +16,23 @@ def simplex(c, Al = None, bl = None, Ag = None, bg = None, Ae = None, be = None,
     if (case is not None):
         x = np.zeros(lc)
         result, table, X, Y, F, ite = simplex_Method(c, table, x, obj, lengths)
+        return result, table, X, Y, F, ite
     elif (Big_M):
         x = np.zeros(lc)
         result, table, X, Y, F, ite = Big_M_Method(c, table, M, x, obj, lengths)
+        return result, table, X, Y, F, ite
     else:
         x = np.zeros(lc)
         table, x_new, C, basic_phs, I, Entering, EI = phase1(c, table, x, obj, lengths)
-        result, table, X, Y, F, ite = phase2(c, table, x_new, obj, C, basic_phs, I, lengths, Entering, EI)
+        if (table[1][-1] > 0):
+            print("No Feasible Solution")
+            result, table, X, Y, F, ite = None, None, None, None, None, None
+            return result, table, X, Y, F, ite
+        else:
+            result, table, X, Y, F, ite = phase2(c, table, x_new, obj, C, basic_phs, I, lengths, Entering, EI)
+            return result, table, X, Y, F, ite
     if Show:
         print("The Optimal Tableau: ")
         print(tabulate(table))
         
-    return result, table, X, Y, F, ite
+    # return result, table, X, Y, F, ite
